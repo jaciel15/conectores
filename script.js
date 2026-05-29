ef66aa966c2e0368d3f7bb89436e00e6cf6d1762
jaciel <jacielf86@gmail.com>
2026-05-29 13:10:14 -0600

    Crear script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+let bar = document.getElementById("loading-bar");
+
+let status = document.getElementById("status");
+
+let menu = document.getElementById("menu");
+
+let splash = document.getElementById("splash");
+
+let percent = 0;
+
+let mensajes = [
+"Cargando módulos...",
+"Preparando escáner...",
+"Inicializando Pinout Engine...",
+"Conectando Base de Datos...",
+"Sistema listo..."
+];
+
+let i = 0;
+
+let timer = setInterval(function(){
+
+percent += 5;
+
+bar.style.width = percent + "%";
+
+if(i < mensajes.length)
+{
+status.innerHTML = mensajes[i];
+i++;
+}
+
+if(percent >= 100)
+{
+clearInterval(timer);
+
+setTimeout(function(){
+
+splash.style.display="none";
+
+menu.style.display="block";
+
+},1000);
+}
+
+},200);
